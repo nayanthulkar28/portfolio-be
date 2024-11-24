e5f4eaab0d94c757ff017a2633d8e31ff13f3834
Nayan Thulkar <nayanthulkar1@gmail.com>
2024-11-24 06:16:35 +0530

    change api general path to /v1

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -9,7 +9,7 @@ import (
 
 func NewRouter(handler *gin.Engine, m *Middleware, eu *usecase.EmailUsecase) {
 	handler.Use(m.CORSMiddleware)
-	h := handler.Group("portfolio-be/v1", m.ValidateApiKey)
+	h := handler.Group("/v1", m.ValidateApiKey)
 	h.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "service is up!!!")
 	})
